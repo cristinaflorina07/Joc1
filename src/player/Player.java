34e092f8470dc30e5731fbcc712e832a27e1865b
Cristina07 <cristinaflorina24@gmail.com>
2024-06-04 11:19:19 +0300

    First commit

---
+++ src/player/Player.java
@@ -0,0 +1,10 @@
+package player;
+
+public class Player {
+    private String username;
+    private double level;
+    private String gen;
+    private double health;
+    private double mana;
+    private double damage;
+}
